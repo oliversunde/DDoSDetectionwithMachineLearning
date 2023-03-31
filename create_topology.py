8d6451409e5ed9381f386dec533ce30d869ae196
oliversunde <83006926+oliversunde@users.noreply.github.com>
2023-03-31 12:12:33 +0200

    Create create_topology.py
    This script sets up a hierarchical Mininet topology with 24 hosts that are connected to edge switches, which are connected to aggregation switches, which are finally connected to the core switch.

---
+++ create_topology.py
@@ -0,0 +1,48 @@
+from mininet.net import Mininet
+from mininet.node import Controller, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def create_topology():
+    net = Mininet(controller=Controller, switch=OVSSwitch)
+
+    info('*** Adding controller\n')
+    net.addController('c0')
+
+    info('*** Adding hosts\n')
+    hosts = []
+    for i in range(1, 25):
+        host = net.addHost(f'h{i}', ip=f'10.0.0.{i}')
+        hosts.append(host)
+
+    info('*** Adding switches\n')
+    edge_switches = [net.addSwitch(f's{i}') for i in range(1, 9)]
+    aggregation_switches = [net.addSwitch(f'a{i}') for i in range(1, 3)]
+    core_switch = net.addSwitch('c1')
+
+    info('*** Creating links\n')
+
+    # Connect hosts to edge switches
+    for i, host in enumerate(hosts):
+        net.addLink(host, edge_switches[i % 8])
+
+    # Connect edge switches to aggregation switches
+    for i, edge_switch in enumerate(edge_switches):
+        net.addLink(edge_switch, aggregation_switches[i % 2])
+
+    # Connect aggregation switches to core switch
+    for agg_switch in aggregation_switches:
+        net.addLink(agg_switch, core_switch)
+
+    info('*** Starting network\n')
+    net.start()
+
+    info('*** Running CLI\n')
+    CLI(net)
+
+    info('*** Stopping network\n')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    create_topology()
