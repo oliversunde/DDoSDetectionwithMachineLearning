74de2c4b0eeac67f07c1fde98424ff618087da42
oliversunde <83006926+oliversunde@users.noreply.github.com>
2023-03-31 12:34:48 +0200

    Update start_tcpdump.sh

--- start_tcpdump.sh
+++ start_tcpdump.sh
@@ -11,3 +11,6 @@ mkdir -p $output_folder
 for i in {1..24}; do
     mnexec -a h$i tcpdump -i h$i-eth0 -w $output_folder/h$i.pcap &
 done
+
+#remember to give it exec rights
+#chmod +x start_tcpdump.sh
